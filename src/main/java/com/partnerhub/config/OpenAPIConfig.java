3feb85e1fcc49f2ba6e14e43cc7db1ad42bf2cf7
Joselito Viveiros Nogueira Filho <joselitofilhoo@gmail.com>
2025-06-15 16:38:08 +0100

    feat: enables basic auth

--- src/main/java/com/partnerhub/config/OpenAPIConfig.java
+++ src/main/java/com/partnerhub/config/OpenAPIConfig.java
@@ -2,6 +2,8 @@ package com.partnerhub.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +13,22 @@ public class OpenAPIConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "basicAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("PartnerHub API")
                         .version("v1")
-                        .description("REST API for managing users and external projects"));
+                        .description("REST API for managing users and external projects"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new io.swagger.v3.oas.models.Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("basic")
+                                )
+                );
     }
 
     @Bean
--- src/main/java/com/partnerhub/config/SecurityConfig.java
+++ src/main/java/com/partnerhub/config/SecurityConfig.java
@@ -1,30 +1,31 @@
-//package com.partnerhub.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+package com.partnerhub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//import static org.springframework.security.config.Customizer.withDefaults;
-//
-//@Configuration
-//public class SecurityConfig {
-////    @Bean
-////    public BCryptPasswordEncoder passwordEncoder() {
-////        return new BCryptPasswordEncoder();
-////    }
-//
-////    @Bean
-////    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-////        http
-////                .csrf(csrf -> csrf.disable())
-////                .authorizeHttpRequests(authz -> authz
-////                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-////                        .anyRequest().authenticated()
-////                )
-////                .with(new HttpBasicConfigurer<>(), withDefaults());
-////        return http.build();
-////    }
-//}
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout((logout) -> logout.permitAll())
+                .with(new HttpBasicConfigurer<>(), withDefaults());
+        return http.build();
+    }
+}
