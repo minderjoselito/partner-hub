91b153799ca1ba1c14096c2cecc6159b7c56499f
Joselito Viveiros Nogueira Filho <joselitofilhoo@gmail.com>
2025-06-17 10:46:18 +0100

    chore: tidy up

--- src/main/java/com/partnerhub/config/SecurityConfig.java
+++ src/main/java/com/partnerhub/config/SecurityConfig.java
@@ -5,18 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
-//    @Bean
-//    public BCryptPasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
